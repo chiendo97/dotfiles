e9c25381cb79a552f69ba5100e641f6d65fb6336
chiendo97 <chiendo97@gmail.com>
2026-03-27 14:33:41 +0700

    feat: add uriel-secrets module with work-related secrets and SSH hosts

---
+++ home-manager/.config/home-manager/modules/uriel-secrets.nix
@@ -0,0 +1,71 @@
+{ config, ... }:
+
+{
+  age.identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519_uriel_dev" ];
+
+  age.secrets = {
+    rclone = {
+      file = ../secrets/rclone.age;
+      path = "${config.home.homeDirectory}/.config/rclone/rclone.conf";
+    };
+    wg_genbook_aws = {
+      file = ../secrets/wg_genbook_aws.age;
+      path = "${config.home.homeDirectory}/.config/wireguard/genbook-aws.conf";
+      mode = "600";
+    };
+    wg_urieljsc_office = {
+      file = ../secrets/wg_urieljsc_office.age;
+      path = "${config.home.homeDirectory}/.config/wireguard/urieljsc-office.conf";
+      mode = "600";
+    };
+  } // builtins.listToAttrs (map (name: {
+    inherit name;
+    value = {
+      file = ../secrets + "/${name}.age";
+      path = "${config.home.homeDirectory}/.ssh/${name}";
+      mode = "600";
+    };
+  }) [
+    "uriel_rsa"
+    "id_ed25519_urieljsc_gitlab"
+    "genbook-mono-deploy"
+    "id_ed25519_github"
+    "id_ed25519_vng_dev"
+  ]);
+
+  programs.ssh.matchBlocks = {
+    "gitlab.com" = {
+      hostname = "gitlab.com";
+      identityFile = "~/.ssh/uriel_rsa";
+      extraOptions = {
+        PreferredAuthentications = "publickey";
+      };
+    };
+
+    "github.com" = {
+      hostname = "github.com";
+      identityFile = "~/.ssh/id_ed25519_github";
+    };
+
+    "vng-dev" = {
+      hostname = "100.64.0.37";
+      port = 234;
+      user = "cle";
+      identityFile = "~/.ssh/id_ed25519_vng_dev";
+    };
+
+    "git.urieljsc.com" = {
+      hostname = "git.urieljsc.com";
+      user = "git";
+      identityFile = "~/.ssh/id_ed25519_urieljsc_gitlab";
+      identitiesOnly = true;
+    };
+
+    "urieljsc" = {
+      hostname = "ssh.urieljsc.com";
+      user = "chienle";
+      identityFile = "~/.ssh/uriel_rsa";
+      identitiesOnly = true;
+    };
+  };
+}
