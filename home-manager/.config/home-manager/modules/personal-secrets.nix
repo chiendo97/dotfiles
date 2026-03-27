04e6b70f7c723371cd4fb95dc7aff502d5081ddd
chiendo97 <chiendo97@gmail.com>
2026-03-27 14:33:49 +0700

    feat: add personal-secrets module with personal secrets and SSH hosts

---
+++ home-manager/.config/home-manager/modules/personal-secrets.nix
@@ -0,0 +1,68 @@
+{ config, lib, ... }:
+
+{
+  age.identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519_agenix" ];
+
+  age.secrets = {
+    api-keys = {
+      file = ../secrets/api-keys.age;
+      path = "${config.home.homeDirectory}/.secrets/api-keys";
+    };
+  } // builtins.listToAttrs (map (name: {
+    inherit name;
+    value = {
+      file = ../secrets + "/${name}.age";
+      path = "${config.home.homeDirectory}/.ssh/${name}";
+      mode = "600";
+    };
+  }) [
+    "aws_bastion_rsa"
+    "github_key"
+    "github_rsa"
+    "cle_viettel_idc"
+    "cle_vpn"
+    "homic_olympus"
+    "homic_rsa"
+    "oracle"
+    "nixos_cle"
+  ]);
+
+  programs.ssh.matchBlocks = {
+    "aws-dev" = {
+      hostname = "10.26.136.50";
+      user = "cle";
+      identityFile = "~/.ssh/aws_bastion_rsa";
+      identitiesOnly = true;
+    };
+
+    "cle-viettel" = {
+      hostname = "171.244.62.91";
+      user = "root";
+      identityFile = "~/.ssh/cle_viettel_idc";
+    };
+
+    "homic-olympus" = {
+      hostname = "100.69.202.110";
+      user = "cle";
+      identityFile = "~/.ssh/homic_olympus";
+    };
+
+    "oracle" = {
+      hostname = "168.138.176.219";
+      user = "ubuntu";
+      identityFile = "~/.ssh/oracle";
+    };
+
+    "nixos-cle" = {
+      hostname = "192.168.50.55";
+      user = "cle";
+      identityFile = "~/.ssh/nixos_cle";
+    };
+  };
+
+  programs.zsh.initContent = ''
+    # API Keys - managed by agenix
+    source ~/.secrets/api-keys 2>/dev/null
+    unset GITHUB_TOKEN
+  '';
+}
